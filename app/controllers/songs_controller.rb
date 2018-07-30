4fc8b886b7f41ac088e7a656bee45e9e86e49614
Elle Faraday <faraday.elle@gmail.com>
2018-07-30 21:22:48 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,9 @@
 class SongsController < ApplicationController
   def index
-    if params[:artist_id]
+    if params[:artist_id] && Artist.find(params[artist_id])
       @songs = Artist.find(params[artist_id]).songs
+    elsif params[:artist_id] && !Artist.find(params[artist_id])
+      
     else
       @songs = Song.all
     end
